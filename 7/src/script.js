80ddc014a00b320dcd4e20f6e3f4f6aa82a27308
Lara Mateo <asdasd5056@gmail.com>
2023-05-10 15:27:13 -0300

    part of 8

--- 7/src/script.js
+++ 7/src/script.js
@@ -22,9 +22,9 @@ const canvas = document.querySelector('canvas.webgl');
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
-};
+  width: window.innerWidth,
+  height: window.innerHeight
+}
 
 // Scene
 const scene = new THREE.Scene();
